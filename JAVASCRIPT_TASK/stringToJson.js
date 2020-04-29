6b4db593705b7e9ce5ab66275f8677698a773503
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-04-29 15:28:21 +0530

    coversion to json

--- JAVASCRIPT_TASK/stringToJson.js
+++ JAVASCRIPT_TASK/stringToJson.js
@@ -21,29 +21,29 @@ const inp = readline.createInterface({
 inp.on("line", (data) => {
 	console.log(json(data))
 });
-function json(string){
-var operators=['||','&&'];
-var array=[];
-string=string.split(' ')
-string.forEach(element => {
-dict={};
-if (operators.includes(element)){
-    if (element=='||'){
-        dict.type='op';
-        dict.value='or';
-        array.push(dict);
-    }
-    else{
-        dict.type='op';
-        dict.value='and';
-        array.push(dict);
-    }
-} 
-else{
-    dict.type='text';
-    dict.value=element;
-    array.push(dict);
-}   
-});
-return array
+function json(string) {
+    var operators = ['||', '&&'];
+    var array = [];
+    string=string.split(' ');
+    string.forEach(element => {
+        dict = {};
+        if (operators.includes(element)) {
+            if (element == '||') {
+                dict.type = 'op';
+                dict.value = 'and';
+                array.push(dict);
+            }
+            else {
+                dict.type = 'op';
+                dict.value = 'or';
+                array.push(dict);
+            }
+        }
+        else {
+            dict.type = 'text';
+            dict.value = element;
+            array.push(dict);
+        }
+    });
+    return array;
 }
